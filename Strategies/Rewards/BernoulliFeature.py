b7d5af0a4c1a7d923047df46a51fb148b1030938
Mazzuco, Diego <Diego.Mazzuco@sabre.com>
2024-01-20 18:32:53 -0300

    mejora simulacion

--- Strategies/Rewards/BernoulliFeature.py
+++ Strategies/Rewards/BernoulliFeature.py
@@ -6,9 +6,10 @@ from matplotlib import pyplot as plt
 
 class BernoulliFeature:
 
-    def __init__(self, k: int, d: int, user_amount: int, cluster_amount, max_prob = 0.5, epsilon=0.01,
+    def __init__(self, k: int, d: int, user_amount: int, cluster_amount,
+                 max_prob = 0.5, epsilon=0.01, cluster_thetas = None,
                  epsilon_cluster=0.01):
-        self.products = np.zeros((k, d))
+        self.contextos = np.zeros((k, d))
         self.probablities = np.zeros((user_amount, k))
         self.best_thetas = np.zeros((user_amount, d))
         self.thetas = np.zeros((user_amount, d))
@@ -19,46 +20,60 @@ class BernoulliFeature:
         self.epsilon = epsilon
         self.epsilon_cluster = epsilon_cluster
         self.max_prob = max_prob
+        if cluster_thetas is not None and len(cluster_thetas) != cluster_amount:
+            raise Exception("La cantidad de thetas de cluster debe ser la misma que la cantidad clusters")
+        self.cluster_thetas = cluster_thetas
         self.init()
 
     def reset(self):
         self.init()
 
+    def get_cluster_thetas(self):
+        if self.cluster_thetas is None:
+            cluster_thetas = np.zeros((self.cluster_amount , self.d))
+            for i in range(self.cluster_amount):
+                cluster_theta = np.random.randn(self.d)
+                cluster_thetas[i] = cluster_theta/np.linalg.norm(cluster_theta)
+            self.cluster_thetas = cluster_thetas
+        return self.cluster_thetas
+
     def init_user(self):
         cluster_size = math.ceil(self.userAmount/self.cluster_amount)
+        cluster_thetas = self.get_cluster_thetas()
         for i in range(self.cluster_amount):
-            cluster_thetas = np.random.randn(self.d)
-            cluster_thetas /= np.linalg.norm(cluster_thetas)
+            cluster_theta = cluster_thetas[i]
             for k in range(cluster_size):
                 count = i*cluster_size + k
                 if count >= self.userAmount:
                     continue
-                theta = cluster_thetas + self.epsilon_cluster * np.random.randn(self.d)
+                theta = cluster_theta + self.epsilon_cluster * np.random.randn(self.d)
                 theta /= np.linalg.norm(theta)
                 self.best_thetas[count, :] = theta
-                self.thetas[count, :] = theta + self.epsilon * np.random.randn(self.d)
+                theta += self.epsilon * np.random.randn(self.d)
+                theta /= np.linalg.norm(theta)
+                self.thetas[count, :] = theta
 
-    def init_articles(self):
+    def init_contexto(self):
         for i in range(self.k):
-            product = np.random.randn(self.d)
-            product /= np.linalg.norm(product)
-            self.products[i, :] = product
+            contexto = np.random.randn(self.d)
+            contexto /= np.linalg.norm(contexto)
+            self.contextos[i, :] = contexto
 
     def init_probabilities(self):
         for u in range(self.userAmount):
             for i in range(self.k):
-                self.probablities[u, i] = np.dot(self.best_thetas[u], self.products[i])
+                self.probablities[u, i] = np.dot(self.best_thetas[u], self.contextos[i])
 
     def init(self):
         self.init_user()
-        self.init_articles()
+        self.init_contexto()
         self.init_probabilities()
 
-    def get_products(self):
-        return self.products
+    def get_contextos(self):
+        return self.contextos
 
     def get_feature(self, a):
-        return self.products[a]
+        return self.contextos[a]
 
     def get_theta(self):
         return self.thetas
@@ -66,10 +81,18 @@ class BernoulliFeature:
     def get_best_thetas(self):
         return self.best_thetas
 
-    def graph(self):
-
+    def graph_best(self):
+        plt.xlim(-1, 1)
+        plt.ylim(-1, 1)
         plt.scatter(self.best_thetas[:, 0], self.best_thetas[:, 1])
         plt.show()
+
+    def graph(self):
+        plt.xlim(-1, 1)
+        plt.ylim(-1, 1)
+        plt.scatter(self.thetas[:, 0], self.thetas[:, 1])
+        plt.show()
+
 
     def get_probabilities(self, user_id: int):
         return self.probablities[user_id, :] * self.max_prob
@@ -84,7 +107,7 @@ class BernoulliFeature:
     def get_reward(self, user_id: int, a: int):
         p = np.random.rand()
         #TODO: esta limitado entre 0 y 1 pero puede ser negativo
-        probability = np.dot(self.thetas[user_id], self.products[a]) * self.max_prob
+        probability = np.dot(self.best_thetas[user_id], self.contextos[a]) * self.max_prob
         if p < probability:
             return 1
         else:
